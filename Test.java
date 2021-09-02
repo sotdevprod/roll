d01fa9a4bf8ca30179e079fc64e0ee07b0efac56
SOT <devprod99@gmail.com>
2021-09-02 11:04:52 +0530

    master

--- Test.java
+++ Test.java
@@ -2,9 +2,6 @@ public class Test
 {
 	public static void main(String...args)
 	{
-		System.out.println("Git");
-		System.out.println("Jenkins!!!");
-		System.out.println("CICD Pipe Line");
-		System.out.println("Development - Production");
+		System.out.println("Git");		
 	}
 }
